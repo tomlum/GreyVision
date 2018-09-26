6f94627a7c25401d4f83e2034caa6421e29488d0
tomlum <checkeredlines@gmail.com>
2018-09-25 23:09:52 -0400

    v 1.0.2 additions/fixes

--- src/inject/injectYoutube.js
+++ src/inject/injectYoutube.js
@@ -1,7 +1,7 @@
 console.log("~Activating Grey Vision~")
 
 new MutationObserver(function(mutations, observer) {
-	document.querySelectorAll(".view-count, .short-view-count, .yt-simple-endpoint.style-scope.yt-formatted-string, #top-level-buttons yt-formatted-string[id='text'], ytd-sentiment-bar-renderer, ytd-badge-supported-renderer, ytd-subscribe-button-renderer span, ytd-comments-header-renderer, count-text, #vote-count-middle, #metadata-line .style-scope").forEach(function(a) {
+	document.querySelectorAll(".view-count, .short-view-count, .super-title .yt-simple-endpoint.style-scope.yt-formatted-string, #top-level-buttons yt-formatted-string[id='text'], ytd-sentiment-bar-renderer, ytd-badge-supported-renderer, ytd-subscribe-button-renderer span, ytd-comments-header-renderer, count-text, #vote-count-middle, #metadata-line .style-scope, #subscriber-count, #subscribers").forEach(function(a) {
 		a.remove()
 	})
 }).observe(document, {
